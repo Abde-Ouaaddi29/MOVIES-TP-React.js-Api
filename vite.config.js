bb93b0a622b6b647bcc4bd919634b7227a255e18
Abde-Ouaaddi29 <abderrahimouaaddi371@gmail.com>
2024-07-03 00:20:49 +0100

    new update

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/MOVIES_TP/',
+// });
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // base: '/MOVIES_TP/',
-});
+  base: '/MOVIES_TP/',
+})
+
 
 
